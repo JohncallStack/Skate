ae13a10669c7948989d5b3caa08db2c06e33159e
JohncallStack <157517331+JohncallStack@users.noreply.github.com>
2024-08-19 16:47:14 +0100

    initial commit

---
+++ backend/test/skate.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Skate from '../lib/skate-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/skate-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Skate.SkateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
